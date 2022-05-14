5310304db5bc17b06f62ac46938a8bb9a3ea7010
Vipin Kumar <88959266+codic96@users.noreply.github.com>
2022-05-14 13:56:39 +0530

    Add files via upload

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,2 +1,25 @@
-#Binary Search Algorithm
+#Binary search in Python
 
+def binarySearch(array,x,low,high):
+    while low <=high:
+        mid = low+(high-low)//2
+
+        if array[mid]==x:
+            return mid
+        elif array[mid]<x:
+            low = mid+1
+        else:
+            high = mid-1
+    return -1
+
+array = [3,4,2,7,8,2,8]
+x=4
+
+result = binarySearch(array,x,0,len(array)-1)
+
+if result != -1:
+    print("Element is present at index"+str(result))
+else:
+
+    print("Not found")
+                           
